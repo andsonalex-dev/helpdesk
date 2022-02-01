b78a2c51803287ed2185899323a64b0be0d6281d
Andson <andson.alex@hotmail.com>
2022-02-01 08:07:21 -0300

    Anotações JPA

--- src/main/java/com/andson/helpdesk/domain/Cliente.java
+++ src/main/java/com/andson/helpdesk/domain/Cliente.java
@@ -3,16 +3,30 @@ package com.andson.helpdesk.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+import com.andson.helpdesk.domain.enums.Perfil;
+
+@Entity
 public class Cliente extends Pessoa {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@OneToMany(mappedBy = "cliente") //informando o tipo de relação e a onde está sendo mapeado
 	private List<Chamado> chamados = new ArrayList<>();
 
 	public Cliente() {
 		super();
+		addPerfil(Perfil.CLIENTE); //sempre que for criado já assume o perfil de cliente
 	}
 
 	public Cliente(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
+		addPerfil(Perfil.CLIENTE); //sempre que for criado já assume o perfil de cliente
 	}
 
 	public List<Chamado> getChamados() {
